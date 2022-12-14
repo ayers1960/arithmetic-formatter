a888f086981ee717dd2752489f3ee5bdfe42751f
Mike Ayers <mgayers@yahoo.com>
2022-12-14 14:36:01 -0500

    time to debug

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -10,21 +10,26 @@ def intCheck(val):
 		return "Error: Numbers cannot be more than four digits."
 	return "" 
 
-def arithmetic_arranger( input ) :
+def arithmetic_arranger( input, showResults=False ) :
 
 	if len(input) > 5:
 		return "Error: Too many problems."
 	
-	pad = "+"*4
+	pad = " "*4
 
 	probs = []
 	for prob in input:
 		probs.append( re.findall("\S+", prob))
 
+	widths = []
+	results = []
+	for (i,p) in enumerate(probs):
+		widths.append( max(len(str(p[0])),len(str(p[2]))) )
+	print(widths)
 	operators = [ '+', '-' ]
 	for p in probs:
 		ret = intCheck(p[0])
-		if ( len(ret) ):
+		if len(ret):
 			return ret
 		ret = intCheck(p[2])
 		if ( len(ret) ):
@@ -32,24 +37,40 @@ def arithmetic_arranger( input ) :
 		if p[1] not in operators:
 			return "Error: Operator must be '+' or '-'."
 
-	for p in probs:
-		print(p[0].rjust(7)+pad,end="")
-	print()
-	for p in probs:
-		print(p[1] + " " + p[2].rjust(5) + pad, end="")
-	print()
-	for p in probs:
-		print('-'*8 +pad,end="")
-	print()
+		if p[1] == "+":
+			results.append(int(p[0])+int(p[2]))
+		else:
+			results.append(int(p[0])-int(p[2]))
+
+
+	answer = ""
+	for (i,p) in enumerate(probs):
+		answer += p[0].rjust(widths[i]+2)+pad
+	answer += "\n"
+
+	for (i,p) in enumerate(probs):
+		answer += p[1] + " " + p[2].rjust(widths[i]) + pad
+	answer += "\n"
+
+	for (i,p) in enumerate(probs):
+		answer += '-'*(widths[i]+2) +pad
+	answer += "\n"
+
+	if showResults:
+		for (i,p) in enumerate(probs):
+			answer += str(results[i]).rjust(widths[i]+2) + pad
+		answer += "\n"
+
+	return answer
 
 def main():
 	answer = arithmetic_arranger([
-						        "1 + 2"
-										,"33 - 44"
-										,"555 + 66"
-										,"7777 - 888"
-										,"7777 - 8"
-										])
+			        "1 + 2"
+				,"33 - 44"
+				,"555 + 45"
+				,"7777 - 888"
+				,"7777 - 8"
+		],True)
 	print(answer)
 
 if __name__ ==  "__main__":
