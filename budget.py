009e040ecdfccaaea971582288bd2e1a585e7d86
ayers1960 <99854257+ayers1960@users.noreply.github.com>
2022-12-15 09:06:04 -0500

    Update budget.py
    class Category created

--- budget.py
+++ budget.py
@@ -1,21 +1,38 @@
 class Category:
   def __init__(self,category):
-    pass
+    self.category = category
+    self.ledger = []
     
-  def deposit(self,amount,description):
-    pass
+  def deposit(self,amount,description=""):
+    self.ledger.append({"amount": amount, "description": description})
   
   def withdraw(self, amount, description = None):
-    pass
+    retVal = False
+    if self.check_funds(amount):
+      amount = -1 * amount;
+      self.ledger.append({"amount": amount, "description": description})
+      retVal = True
+    return retVal
   
   def get_balance(self):
-    pass
+    balance = 0
+    for r in self.ledger:
+      balance += float(r['amount'])
+    return balance
   
   def transfer(self, amount, category ):
-    pass
+    retVal = False
+    if self.check_funds(amount):
+      description = f"Transfer to {category}"
+      self.ledger.append({"amount": -1*amount, "description": description})
+      description = f"Transfer from {self.category}"
+      self.deposit(amount, description)
+      retVal = True
+    return retVal
   
   def check_funds(self, amount):
-    pass
+    balance = self.get_balance()
+    return (amount <= balance)
 
 
 
