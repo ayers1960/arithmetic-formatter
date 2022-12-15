640a650a4e138d23cc5642933a3c1bdc47f1a4bf
ayers1960 <99854257+ayers1960@users.noreply.github.com>
2022-12-15 10:12:13 -0500

    Update budget.py
    all but create_spend_chart

--- budget.py
+++ budget.py
@@ -2,6 +2,24 @@ class Category:
   def __init__(self,category):
     self.category = category
     self.ledger = []
+
+  def __str__(self):
+    titleSize = 30
+    descSize  = 23
+    catLen = len(self.category)
+    left = (titleSize - catLen)//2
+    
+    right = (titleSize - catLen)//2
+    rightStr = "*"*right
+    leftStr  = "*"*left
+    retStr = f"{leftStr}{self.category}{rightStr}\n" 
+    for r in self.ledger:
+      retStr += f"{r['description'][0:descSize]:{descSize}s}"
+      retStr += f"{float(r['amount']):7.2f}"
+      retStr += "\n"
+    balance = self.get_balance()
+    retStr += f"Total: {balance:.2f}"
+    return retStr
     
   def deposit(self,amount,description=""):
     self.ledger.append({"amount": amount, "description": description})
@@ -10,6 +28,8 @@ class Category:
     retVal = False
     if self.check_funds(amount):
       amount = -1 * amount;
+      if description is None:
+        description = ""
       self.ledger.append({"amount": amount, "description": description})
       retVal = True
     return retVal
@@ -23,10 +43,10 @@ class Category:
   def transfer(self, amount, category ):
     retVal = False
     if self.check_funds(amount):
-      description = f"Transfer to {category}"
+      description = f"Transfer to {category.category}"
       self.ledger.append({"amount": -1*amount, "description": description})
       description = f"Transfer from {self.category}"
-      self.deposit(amount, description)
+      category.deposit(amount, description)
       retVal = True
     return retVal
   
