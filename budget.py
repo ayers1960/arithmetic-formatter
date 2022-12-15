a1a349270146be4f2bdc3dc988d27f8e4688aeed
ayers1960 <99854257+ayers1960@users.noreply.github.com>
2022-12-15 08:46:53 -0500

    Create budget.py

---
+++ budget.py
@@ -0,0 +1,23 @@
+class Category:
+  def __init__(self,category):
+    pass
+    
+  def deposit(self,amount,description):
+    pass
+  
+  def withdraw(self, amount, description = None):
+    pass
+  
+  def get_balance(self):
+    pass
+  
+  def transfer(self, amount, category ):
+    pass
+  
+  def check_funds(self, amount):
+    pass
+
+
+
+def create_spend_chart(categories):
+  pass
